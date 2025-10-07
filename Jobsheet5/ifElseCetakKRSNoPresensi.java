1ecfb9daabcf29566933dfa9512ec581e91ff68c
23weqsszdzgv <tegr12q@gmail.com>
2025-10-07 12:57:49 +0700

    modifikasi percobaan if-else

---
+++ Jobsheet5/ifElseCetakKRSNoPresensi.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ifElseCetakKRSNoPresensi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan semester saat ini: ");
+        int semester = sc.nextInt();
+
+        if (semester == 1) {
+            System.out.println("Menampilkan KRS Semester 1");
+        } else if (semester == 2) {
+            System.out.println("Menampilkan KRS Semester 2");
+        } else if (semester == 3) {
+            System.out.println("Menampilkan KRS Semester 3");
+        } else if (semester == 4) {
+            System.out.println("Menampilkan KRS Semester 4");
+        } else if (semester == 5) {
+            System.out.println("Menampilkan KRS Semester 5");
+        } else if (semester == 6) {
+            System.out.println("Menampilkan KRS Semester 6");
+        } else if (semester == 7) {
+            System.out.println("Menampilkan KRS Semester 7");
+        } else if (semester == 8) {
+            System.out.println("Menampilkan KRS Semester 8");
+        } else {
+            System.out.println("Semester tidak valid.");
+        }
+    }
+}
